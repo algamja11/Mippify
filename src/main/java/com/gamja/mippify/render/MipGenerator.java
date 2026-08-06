0674aab00b3e53023cd3fb1cab160c4683f04bdd
Gamja <142703479+algamja11@users.noreply.github.com>
2026-08-07 01:52:19 +0900

    Custom blending is not needed anymore

--- src/main/java/com/gamja/mippify/render/MipGenerator.java
+++ src/main/java/com/gamja/mippify/render/MipGenerator.java
@@ -88,14 +88,10 @@ public class MipGenerator {
                             int color4 = lastData.getPixel(x * 2 + 1, y * 2 + 1);
 
                             int color;
-                            if (Mippify.config().smoothing) {
-                                color = alphaBlend(color1, color2, color3, color4);
+                            if (mipmapStrategy == MipmapStrategy.DARK_CUTOUT) {
+                                color = darkenedAlphaBlend(color1, color2, color3, color4);
                             } else {
-                                if (mipmapStrategy == MipmapStrategy.DARK_CUTOUT) {
-                                    color = darkenedAlphaBlend(color1, color2, color3, color4);
-                                } else {
-                                    color = ARGB.meanLinear(color1, color2, color3, color4);
-                                }
+                                color = ARGB.meanLinear(color1, color2, color3, color4);
                             }
 
                             data.setPixel(x, y, color);
@@ -105,7 +101,7 @@ public class MipGenerator {
                     result[level] = data;
                 }
 
-                if (isCutoutMip && !Mippify.config().smoothing) {
+                if (isCutoutMip) {
                     scaleAlphaToCoverage(result[level], originalCoverage, cutoutRef, alphaCutoffBias);
                 }
             }
@@ -116,42 +112,6 @@ public class MipGenerator {
 
     private static int darkenedAlphaBlend(final int color1, final int color2, final int color3, final int color4) {
         return (int) ReflectionUtils.tryInvoke(DARKENED_ALPHA_BLEND, null, color1, color2, color3, color4);
-    }
-
-    private static int alphaBlend(int c1, int c2, int c3, int c4) {
-        int cx1 = alphaBlend(c1, c2);
-        int cx2 = alphaBlend(c3, c4);
-        return alphaBlend(cx1, cx2);
-    }
-
-    private static int alphaBlend(int c1, int c2) {
-        int a1 = ARGB.alpha(c1);
-        int a2 = ARGB.alpha(c2);
-        int a = (a1 + a2) / 2;
-        if (a1 == 0 && a2 == 0) {
-            a1 = 1;
-            a2 = 1;
-        } else {
-            if (a1 == 0) {
-                c1 = c2;
-                a = a2;
-            }
-            if (a2 == 0) {
-                c2 = c1;
-                a = a1;
-            }
-        }
-        int r1 = ARGB.red(c1) * a1;
-        int g1 = ARGB.green(c1) * a1;
-        int b1 = ARGB.blue(c1) * a1;
-        int r2 = ARGB.red(c2) * a2;
-        int g2 = ARGB.green(c2) * a2;
-        int b2 = ARGB.blue(c2) * a2;
-        int r = (r1 + r2) / (a1 + a2);
-        int g = (g1 + g2) / (a1 + a2);
-        int b = (b1 + b2) / (a1 + a2);
-
-        return ARGB.color(a, r, g, b);
     }
 
     public static void fixTransparentColor(NativeImage image) {
